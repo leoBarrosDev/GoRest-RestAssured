c5a7b921623cc1946b82bec9e9bebf3bc076cde0
Leandro Reis <leohbr@gmail.com>
2024-02-29 17:39:27 -0300

    add resources const

--- src/test/java/TestGoRest.java
+++ src/test/java/TestGoRest.java
@@ -6,6 +6,10 @@ import static io.restassured.RestAssured.given;
 public class TestGoRest {
 
     private final String BASE_URL = "https://gorest.co.in/public-api";
+    private final String USER_RESOURCE = "/users";
+    private final String POST_RESOURCE = "/posts";
+    private final String COMMENT_RESOURCE = "/comments";
+    private final String TODO_RESOURCE = "/todos";
 
     @Test
     @DisplayName("Deve listar todos os usuários cadastrados na base de dados")
