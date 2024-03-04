607b27c80452111783c2445e585cb063cdd315d2
Leandro Reis <leohbr@gmail.com>
2024-03-04 16:09:16 -0300

    add update and delete user tests

--- src/test/java/TestGoRest.java
+++ src/test/java/TestGoRest.java
@@ -2,6 +2,9 @@ import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,15 +20,17 @@ public class TestGoRest {
     String COMMENT_RESOURCE = "/comments";
     String TODO_RESOURCE = "/todos";
 
-    @Test
-    @DisplayName("Deve listar todos os usuários cadastrados na base de dados")
-    public void listUsers() {
-
+    @BeforeEach
+    public void sslConfig() {
         RestAssured.config = RestAssuredConfig.config().sslConfig(
                 new SSLConfig().relaxedHTTPSValidation()
         );
+    }
 
-        String response = "{}";
+
+    @Test
+    @DisplayName("Deve listar todos os usuários cadastrados na base de dados")
+    public void listUsers() {
 
         given()
                 .contentType(ContentType.JSON)
@@ -51,8 +56,8 @@ public class TestGoRest {
                 new SSLConfig().relaxedHTTPSValidation()
         );
         String newClient = "{\n" +
-                "    \"name\": \"Agora Vai Barros\",\n" +
-                "    \"email\": \"simbora@gmail.com\",\n" +
+                "    \"name\": \"Novo Teste\",\n" +
+                "    \"email\": \"lala@gmail.com\",\n" +
                 "    \"gender\": \"male\",\n" +
                 "    \"status\": \"active\"\n" +
                 "}";
@@ -74,5 +79,79 @@ public class TestGoRest {
                 .body("code", equalTo(201))
                 .log().all();
 
+    }
+
+    @Test
+    @DisplayName("Deve atualizar um usuário baseado no ID informado como parâmetro e considerando os dados passados no body")
+    public void updateUser() {
+        RestAssured.config = RestAssuredConfig.config().sslConfig(
+                new SSLConfig().relaxedHTTPSValidation()
+        );
+
+        String newClient = "{\n" +
+                "    \"name\": \"lele Teste\",\n" +
+                "    \"email\": \"lelel123123e@gmail.com\",\n" +
+                "    \"gender\": \"male\",\n" +
+                "    \"status\": \"active\"\n" +
+                "}";
+
+        String newDataClient = "{\n" +
+                "    \"name\": \"Leandro H. de B. Reis\",\n" +
+                "    \"email\": \"leohbr12345@gmail.com\",\n" +
+                "    \"gender\": \"Male\",\n" +
+                "    \"status\": \"active\"\n" +
+                "}";
+
+        Response responseCreateUser = given()
+                .header("Authorization", "Bearer " + token)
+                .body(newClient)
+                .contentType(ContentType.JSON)
+                .when()
+                .post(BASE_URL + USER_RESOURCE);
+
+        Integer userId = responseCreateUser.path("data.id");
+
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .body(newDataClient)
+                .contentType(ContentType.JSON)
+                .when()
+                .put(BASE_URL + USER_RESOURCE + "/" + userId)
+                .then()
+                .statusCode(200)
+                //.body(containsString(newDataClient))
+                .body("code", equalTo(200))
+                .log().all();
+    }
+
+    @Test
+    @DisplayName("Deve excluir um usuário na base de dados baseado no id passado como parâmetro")
+    public void deleteUser() {
+
+        String newClient = "{\n" +
+                "    \"name\": \"Novo Teste\",\n" +
+                "    \"email\": \"la159la@gmail.com\",\n" +
+                "    \"gender\": \"male\",\n" +
+                "    \"status\": \"active\"\n" +
+                "}";
+        Response responseCreateUser = given()
+                .header("Authorization", "Bearer " + token)
+                .body(newClient)
+                .contentType(ContentType.JSON)
+                .when()
+                .post(BASE_URL + USER_RESOURCE);
+
+        Integer userId = responseCreateUser.path("data.id");
+
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .delete(BASE_URL + USER_RESOURCE + "/" + userId)
+                .then()
+                .statusCode(200)
+                //.body("code", equalTo(204))
+                .log().all();
     }
 }
